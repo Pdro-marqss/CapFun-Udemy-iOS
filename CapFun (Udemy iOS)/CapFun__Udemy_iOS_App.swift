069d6e2d2156a78ce211c6aa268d160e2dbdd1b3
Pedro Marques <pdro.marqs2015@hotmail.com>
2024-04-11 12:13:21 -0300

    Initial Commit

---
+++ CapFun (Udemy iOS)/CapFun__Udemy_iOS_App.swift
@@ -0,0 +1,17 @@
+//
+//  CapFun__Udemy_iOS_App.swift
+//  CapFun (Udemy iOS)
+//
+//  Created by Pedro Marques on 11/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CapFun__Udemy_iOS_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
